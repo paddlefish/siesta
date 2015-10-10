5903ea650968cccf439471ceb8c3d6cab7b799c0
Paul Cantrell <cantrell@pobox.com>
2015-10-09 23:07:59 -0500

    CI is not actually worth this. I am just obsessed.

--- Tests/ResourceSpecBase.swift
+++ Tests/ResourceSpecBase.swift
@@ -27,7 +27,7 @@ class ResourceSpecBase: SiestaSpec
             {
             // Nocilla’s threading is broken, and Travis exposes a race condition in it.
             // This delay is a workaround.
-            afterEach { NSThread.sleepForTimeInterval(0.05) }
+            afterEach { NSThread.sleepForTimeInterval(0.1) }
             }
         
         beforeSuite { LSNocilla.sharedInstance().start() }
